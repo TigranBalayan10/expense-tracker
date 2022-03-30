1f613f4dbde6b2228dd1ffcfde9a500c6abb0d38
Tokhalyan <tokhalyan@gmail.com>
2022-03-30 12:58:39 -0700

    api routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { Product, Tag, User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password,
+        monthly_income: req.body.monthly_income
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
-// get all posts for homepage
+// // get all posts for homepage
 router.get('/', (req, res) => {
-  res.send('Hello World')
+    res.send('Hello World')
 });
+
 
 
 module.exports = router;
