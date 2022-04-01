dd2a7d3969ac123abd7cefff2d6708e5b380a535
Anthony Barragan <anthony.bar.89@gmail.com>
2022-03-31 21:47:28 -0700

    charts respond with individual users

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -31,17 +31,22 @@ router.get('/', (req, res) => {
 })
 
 // Find total $ in products in a given Tag
-router.get('/total/:tag_id', (req, res) => {
+router.get('/total/:tag_id/:user_id', (req, res) => {
     Product.findAll({
         where: {
-            tag_id: req.params.tag_id
+            tag_id: req.params.tag_id,
+            user_id: req.params.user_id
         },
         attributes: [
             [sequelize.fn('sum', sequelize.col('price')), 'total_price']
         ]
     })
-    .then(dbProductData => res.json(dbProductData))
-});
+    .then(dbProductData => {
+        console.log(dbProductData);
+        res.json(dbProductData);
+    })
+    .catch(err => res.json(err))
+})
 
 // Find one product 
 router.get('/:id', (req, res) => {
