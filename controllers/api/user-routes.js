4e431ab6092ddcf8de45c5cf6b16d54f8a1c1089
Tokhalyan <tokhalyan@gmail.com>
2022-04-03 03:06:02 -0700

    all home-page routes working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,52 +1,9 @@
 const router = require('express').Router();
 const { Product, Tag, User } = require('../../models');
-
-// Get all users
-router.get('/', (req, res) => {
-    User.findAll({
-        attributes: { exclude: ['password'] },
-        include: {
-            model: Product
-        }
-    })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-// Get one user
-router.get('/:id', (req, res) => {
-    User.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: { exclude: ['password'] },
-        include: [
-            {
-                model: Product,
-            },
-            {
-                model: Tag
-            }
-    ],
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-        }
-        res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-})
+const withAuth = require('../../utils/auth');
 
 // Create User
-router.post('/', (req, res) => {
+router.post('/signup', (req, res) => {
     User.create({
         username: req.body.username,
         password: req.body.password,
@@ -91,7 +48,6 @@ router.post('/login', (req, res) => {
                 req.session.loggedIn = true;
 
                 res.json({
-                    user: dbUserData,
                     message: 'You are now logged in'
                 });
             });
@@ -102,14 +58,10 @@ router.post('/login', (req, res) => {
         });
 })
 
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
+router.post('/logout', withAuth, (req, res) => {
+    req.session.destroy(() => {
+        res.status(204).end();
+    });
 })
 
 // Update User
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,30 +1,28 @@
 async function signupFormHandler(event) {
     event.preventDefault();
-  
-    const username = document.querySelector("#floatingInput").value.trim();
-    const income = document.querySelector("#floatiingIncome").value.trim();
-    const password = document.querySelector("#floatingPassword").value.trim();
-    console.log(username, income, password);
-    if (username && income && password) {
-      const response = await fetch("/signup", {
-        method: "post",
-        body: JSON.stringify({
-          username,
-          password,
-          income,
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
-      console.log(response);
-      if (response.ok) {
-        // document.location.replace("/dashboard/");
-        alert("Signup successful");
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
 
-  document
-  .querySelector("#signup-form")
-  .addEventListener("click", signupFormHandler);
+    const username = document.querySelector("#floatingInput").value.trim();
+    const monthly_income = document.querySelector("#floatingIncome").value.trim();
+    const password = document.querySelector("#floatingPassword").value.trim();
+    console.log(username, monthly_income, password);
+    if (username && monthly_income && password) {
+        const response = await fetch("api/users/signup", {
+            method: "post",
+            body: JSON.stringify({
+                username,
+                password,
+                monthly_income,
+            }),
+            headers: { "Content-Type": "application/json" },
+        });
+        console.log(response);
+        if (response.ok) {
+            document.location.replace("/dashboard/");
+            alert("Signup successful");
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector("#signup-form").addEventListener("click", signupFormHandler);
