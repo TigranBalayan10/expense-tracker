6530ace8b8b119eff146e0e54e59ecefee99b7f7
Anthony Barragan <anthony.bar.89@gmail.com>
2022-04-03 10:32:44 -0700

    adding user routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -23,9 +23,9 @@ router.get('/signup', (req, res) => {
 });
 
 // Route to the Dashboard
-// router.get('/dashboard', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/html/piechart.html'));
-// });
+router.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/html/piechart.html'));
+});
 
 // // get all posts for homepage
 // router.get('/', (req, res) => {
@@ -61,9 +61,9 @@ router.post('/', (req, res) => {
 });
 
 // login page
-router.get('/dashboard', withAuth, (req, res) => {
-  res.render('dashboard', { income : `$3400`, expenses: `$1356` });
-});
+// router.get('/dashboard', withAuth, (req, res) => {
+//   res.render('dashboard', { income : `$3400`, expenses: `$1356` });
+// });
 
 
 
